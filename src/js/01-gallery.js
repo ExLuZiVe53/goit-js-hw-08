1daac4febfbdef5d78b41522d0365079b109b5c9
ExLuZiVe53 <imstudent27@gmail.com>
2023-07-30 13:55:08 +0200

    add galery js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+// Імпорт бібліотеки з документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів з документації
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+console.log('Hello');
+const galleryEl = document.querySelector('.gallery');
+
+gallerySet(createGalleryMarkup(galleryItems));
+console.log(gallerySet(createGalleryMarkup(galleryItems)));
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ description, preview, original }) =>
+        `<li class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+    <img class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+    </a>
+    </li>`
+    )
+    .join('');
+}
+
+function gallerySet(gallery) {
+  galleryEl.innerHTML = gallery;
+}
