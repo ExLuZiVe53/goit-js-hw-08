817bb78e373f76e29eb07f12ee24b3316f0fc2f6
ExLuZiVe53 <imstudent27@gmail.com>
2023-07-30 20:27:12 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const CURRENT_TIME = 'videoplayer-current-time';
+let currentTime = localStorage.getItem(CURRENT_TIME || 0);
+
+const player = new Player(iframe);
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+setTimePlayer(currentTime);
+
+function getCurrentTime(data) {
+  currentTime = data.seconds;
+  localStorage.setItem('CURRENT_TIME', data.seconds);
+}
+
+function setTimePlayer(time) {
+  player.setCurrentTime(time);
+}
