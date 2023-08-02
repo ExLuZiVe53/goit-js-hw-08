7d2e2de302f55b66217151c4df8e909e614611e6
ExLuZiVe53 <imstudent27@gmail.com>
2023-08-02 10:10:24 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,50 +1,42 @@
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector('.feedback-form');
-const input = form.email;
-const textArea = form.message;
-const userData = {
-  email: '',
-  message: '',
+const feedbackForm = document.querySelector('.feedback-form');
+feedbackForm.addEventListener('input', throttle(onInput, 500));
+feedbackForm.addEventListener('submit', onSubmitForm);
+
+const FEEDBACK_FORM_STALE = 'feedback-form-state';
+let data = {};
+
+const inData = () => {
+  try {
+    const dataOnJSON = localStorage.getItem(FEEDBACK_FORM_STALE);
+    if (!dataOnJSON) {
+      return;
+    }
+    data = JSON.parse(dataOnJSON);
+    Object.entries(data).forEach(([key, value]) => {
+      feedbackForm.elements[key].value = value;
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
 };
 
-form.addEventListener('input', throttle(onInput, 500));
+window.addEventListener('load', inData);
+
 function onInput(event) {
-  const userData = {
-    email: input.value.trim(),
-    message: event.currentTarget.elements.message.value.trim(),
-  };
-
-  localStorage.setItem('feedback-form-state', JSON.stringify(userData));
+  data[event.target.name] = event.target.value.trim();
+  setLocalStorage();
 }
 
-// Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми.
-if (JSON.parse(localStorage.getItem('feedback-form-state'))) {
-  const localData = JSON.parse(localStorage.getItem('feedback-form-state'));
-  input.value = Object.values(localData)[0];
-  textArea.value = Object.values(localData)[1];
-}
-
-// Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
-
-form.addEventListener('submit', handleSubmit);
-
-function handleSubmit(event) {
+function onSubmitForm(event) {
   event.preventDefault();
+  data = {};
+  console.log(data);
+  feedbackForm.reset();
+  localStorage.removeItem(FEEDBACK_FORM_STALE);
+}
 
-  const {
-    elements: { email, message },
-  } = event.currentTarget;
-
-  if (email.value.trim() === '' || message.value.trim() === '') {
-    return alert("Заповніть, будь ласка, обов'язково всі поля!");
-  } else {
-    localStorage.removeItem('feedback-form-state');
-    console.log({
-      email: email.value.trim(),
-      message: message.value.trim(),
-    });
-    input.value = '';
-    textArea.value = '';
-  }
+function setLocalStorage() {
+  localStorage.setItem(FEEDBACK_FORM_STALE, JSON.stringify(data));
 }
